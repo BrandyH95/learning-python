c0147f8ddb0dfd4c2f24e8b046ccd6514aed4c41
Brandy Brewer <33636081+BrandyH95@users.noreply.github.com>
2022-12-31 17:17:11 -0500

    print cleanup

--- rock-paper-scissors/main.py
+++ rock-paper-scissors/main.py
@@ -17,7 +17,7 @@ while True:
     random_number = random.randint(0, 2)
     #rock: 0, paper: 1, scissors: 2
     computer_pick = options[random_number]
-    print("Computer picked: ", computer_pick + ".")
+    print("Computer picked", computer_pick + ".")
 
     if user_input == "rock" and computer_pick == "scissors":
         print("You won!")
